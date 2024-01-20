8198d4c8d632cb5684b77fee82489460d6996f79
CodeNinjasEtobicoke <148917437+iam9116@users.noreply.github.com>
2024-01-20 12:15:15 -0500

    C# Review

---
+++ Assets/TestScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public GameObject obj;
+    public int gridSize = 5;
+
+    Vector3 position;
+    float spacing = 1f;
+
+    private void Start()
+    {
+        position = new Vector3(-5f, 0f, 0f);
+
+
+        func(); 
+    }
+
+    public void func()
+    {
+        if (obj == null) return;
+
+        for (int j = 0; j < 4; j++)
+        {
+            for (int i = 0; i <= 8; i++)
+            {
+                Instantiate(obj, position, Quaternion.identity);
+
+                position.x += i;
+                position.y += 2;
+
+                Debug.Log(i);
+            }
+        }
+    }
+}
